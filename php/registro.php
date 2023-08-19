e6f936635139d327f3239e06b2e7b977e1bcb372
kevincamayo98 <142462616+kevincamayo98@users.noreply.github.com>
2023-08-19 18:08:03 -0500

    modificacion registro.php

--- php/registro.php
+++ php/registro.php
@@ -1,16 +1,22 @@
-
 <?php
   
        include ('cone.php');
 
-       $sql = "INSERT INTO usuario (nombre, usuario, correo, clave) VALUES ('Test', 'Testing', 'Testing@tesing.com', '123')";
-       
-        if (mysqli_query($mysqli, $sql)) {
-            echo "New record created successfully";
+        // Guardar los datos recibidos del formulario
+        $nombre = $_POST['nombre'];
+        $usuario = $_POST['usuario'];
+        $correo = $_POST['correo'];
+        //clave encriptada
+        $clave = password_hash($_POST['clave'], PASSWORD_DEFAULT);
+        
+       $sql = "INSERT INTO usuarios (nombre, usuario, correo, clave) VALUES ('$nombre', '$usuario', '$correo', '$clave')";
+        
+        if (mysqli_query($cone, $sql)) {
+            echo "Usuario registrado correctamente";
         } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($cone);
-        }
-        mysqli_close($cone);
+        
 
+            }
+        
 ?>
-
